72222972d8d95a47724fe198775ed221f58f317e
endxkartikey <endxkartikey@gmail.com>
2025-05-24 11:54:56 +0530

    Add all project files

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,23 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',
+
   plugins: [
     react(),
     tailwindcss(),
   ],
+
   assetsInclude: ['**/*.wasm'],
+
   optimizeDeps: {
     exclude: ['@electric-sql/pglite'],
   },
+
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+
+ 
 })
